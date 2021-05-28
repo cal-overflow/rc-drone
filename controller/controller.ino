8f8cf5e5d7e3688730ab58923822857dcdb5e5d3
Christian Lisle <lisleachristian@gmail.com>
2021-05-27 22:32:51 -0500

    Wrote very simple controller (transmitter) and drone (receiver) scripts.

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,9 +1,29 @@
-// Transmitter
+// Transmitter (Controller)
+#include <RH_ASK.h>
+#include <SPI.h>
+RH_ASK controller;
+
+const int potentiometerPO = A0;
+int p = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); // debugging purposes only
+
+  if (!controller.init()) {
+    Server.println("Failed to initialize controller (transmitter)"); // debugging purposes only
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int newP = analogRead(potentiometerPO);
 
+  // Only send new message if potentiometer value changed
+   if (newP != p)  {
+    p = newP;
+    
+    controller.send((uint8_t *)p, sizeof(p));
+    controller.waitPacketSent();
+    
+    delay(50);
+   }
 }
--- drone/drone.ino
+++ drone/drone.ino
@@ -1,30 +1,48 @@
-// Receiver
+// Receiver (Drone)
+#include <RH_ASK.h>
+#include <SPI.h>
 #include <Servo.h>
+
+RH_ASK drone;
 Servo servo, ESC;
-int sRange[2]={0, 180};
-int escRange[2]={1000, 2000};
+
+const int servoPO = A5;
+const int escPO = A5;
+
+const int sRange[2]={0, 180};
+const int escRange[2]={1000, 2000};
+uint8_t data;
+uint16_t p;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // debugging purposes only
 
-  //servo.attach(A5, sRange[0], sRange[1]);
-  ESC.attach(A5, escRange[0], escRange[1]);
-  //servo.write(sRange[0]);
-  ESC.write(escRange[1]);
+
+  if (!drone.init())
+    Server.println("Failed to initialize drone (receiver)"); // debugging purposes only
+
+  servo.attach(servoPO, sRange[0], sRange[1]);
+  //ESC.attach(escPO, escRange[0], escRange[1]);
+  servo.write(sRange[0]);
+  //ESC.write(escRange[1]);
 } 
 
 void loop() {
-  /*int p = analogRead(A3); // Potentiometer
-  Serial.println(p);
-  p = map(p, 0, 1023, sRange[0], sRange[1]);
-  servo.write(p);*/
+  uint8_t buf = sizeof(data);
+  if (drone.recv((uint8_t*)&data, &datalen) && datalen == sizeof(data)) {
+    uint16_t p = data;
 
-  int p = analogRead(A3); // Potentiometer
-  Serial.println(p);
-  p += 120; // Reduce deadzone
-  p = map(p, 0, 1023, escRange[0], escRange[1]);
-  ESC.write(p);
+    // debugging purposes only:
+    Serial.print("Received:");
+    Serial.println(p);
+  }
   
-  //delay(100);
+  p += 120; // Reduce deadzone
+  p = map(p, 0, 1023, servoRange[0], servoRange[1]);
+  servo.write(p);
+  
+  //p = map(p, 0, 1023, escRange[0], escRange[1]);
+  //ESC.write(p);
+
   
 }
