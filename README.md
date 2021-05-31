60c34cf8af8f0104a85fc1d23651631507c2f2af
Christian Lisle <lisleachristian@gmail.com>
2021-05-30 20:54:56 -0500

    Updated README and worked on transmitter/receiver

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 Designing a RC Airplane over the 2021 summer break.
 
+### Arduino Libraries needed:
+
+* Servo
+
+* RadioHead
+
+[Official Arduino documentation for installing libraries](https://www.arduino.cc/en/guide/libraries)
+
+## Pin-Out requirements:
+
+**Transmitter**: must be connected to pin 12
+
+**Receiver**: must be connected to pin 11
+
+
 ## Contributors
 
 [Spencer Dunham](https://github.com/Sdunham01)
--- controller/controller.ino
+++ controller/controller.ino
@@ -5,25 +5,44 @@ RH_ASK controller;
 
 const int potentiometerPO = A0;
 int p = 0;
+char data[3];
 
 void setup() {
   Serial.begin(9600); // debugging purposes only
 
   if (!controller.init()) {
-    Server.println("Failed to initialize controller (transmitter)"); // debugging purposes only
+    Serial.println("Failed to initialize controller (transmitter)"); // debugging purposes only
   }
 }
 
 void loop() {
   int newP = analogRead(potentiometerPO);
+  
 
   // Only send new message if potentiometer value changed
-   if (newP != p)  {
-    p = newP;
-    
-    controller.send((uint8_t *)p, sizeof(p));
-    controller.waitPacketSent();
-    
-    delay(50);
-   }
+   //if (newP != p)  {
+  //newP += 120; // Reduce deadzone
+  newP = map(newP, 0, 1023, 0, 255);
+  p = newP;
+  //Serial.println(p);
+
+   data[0] = '\0';
+   data[1] = '\0';
+   data[2] = '\0';
+  sprintf(data, "%d", p);
+  //itoa(p, data, 10);
+  //data[3] = 'a';
+  //data[4] ='\0';
+  //for (int i = 0; i < strlen(data); i++)  {
+    //Serial.print(data[i]);
+  //}
+  Serial.println(strlen(data));
+  controller.send((uint8_t *)data, strlen(data));
+  controller.waitPacketSent();
+  
+  delay(50);
+   //}
+   data[0] = '0';
+   data[1] = '0';
+   data[2] = '0';
 }
