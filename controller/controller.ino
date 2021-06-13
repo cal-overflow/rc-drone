f078fb3144b7486406bae9e3e7125f3e755633f4
Gautham Ajith <--global>
2021-06-13 18:25:13 -0500

    Sending message works

--- controller/controller.ino
+++ controller/controller.ino
@@ -26,23 +26,47 @@ void loop() {
   p = newP;
   //Serial.println(p);
 
-   data[0] = '\0';
-   data[1] = '\0';
-   data[2] = '\0';
   sprintf(data, "%d", p);
+//  char newArray[sizeof(data)];
+//  int j = sizeof(newArray)-1;
+//  for(int i = sizeof(data)-1; i >= 0; i--)
+//  {
+//      newArray[j-i] = data[i];
+//      
+//  }
+//  Serial.println(strlen(consArray));
   //itoa(p, data, 10);
   //data[3] = 'a';
   //data[4] ='\0';
   //for (int i = 0; i < strlen(data); i++)  {
     //Serial.print(data[i]);
   //}
-  Serial.println(strlen(data));
-  controller.send((uint8_t *)data, strlen(data));
+  //Serial.println(strlen(data));
+  //const char *msg = "Heloo World";
+  char consArray[4];
+  if(strlen(data) < 3 && strlen(data) >1){
+    consArray[0] ='0';
+    consArray[1] = data[0];
+    consArray[2] = data[1];
+    consArray[3] = 'a';
+  }
+  else if(strlen(data)< 2){
+    consArray[0] ='0';
+    consArray[1] = '0';
+    consArray[2] = data[0]; 
+    consArray[3] = 'a';
+  }
+  else{
+    for(int i =0; i < strlen(data); i++){
+      consArray[i] = data[i];
+      }
+      consArray[3] = 'a';
+  }
+
+  Serial.println(consArray);
+  controller.send((uint8_t *)consArray, strlen(consArray));
   controller.waitPacketSent();
   
   delay(50);
-   //}
-   data[0] = '0';
-   data[1] = '0';
-   data[2] = '0';
+
 }
