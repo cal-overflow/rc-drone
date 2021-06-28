4761c50455cfa2f568a58e15e22564e3e203663b
Christian Lisle <lisleachristian@gmail.com>
2021-06-27 20:42:29 -0500

    Successfully controlled Servo remotely

--- drone/drone.ino
+++ drone/drone.ino
@@ -1,13 +1,12 @@
 // Receiver (Drone)
-// #include <RH_ASK.h>
+#include <RH_ASK.h>
 #include <SPI.h>
-// #include <PWMServo.h>
-#include <Servo.h>
+#include <PWMServo.h>
 
-// RH_ASK drone;
-Servo servo, ESC;
+RH_ASK drone;
+PWMServo servo, ESC;
 
-const int servoPO = A5;
+const int servoPO = 9;
 const int escPO = A5;
 
 const int sRange[2]={0, 180};
@@ -17,38 +16,37 @@ uint16_t p;
 void setup() {
   Serial.begin(9600); // debugging purposes only
 
-//   if (!drone.init())
-//    Serial.println("Failed to initialize drone (receiver)"); // debugging purposes only
+   if (!drone.init())
+    Serial.println("Failed to initialize drone (receiver)"); // debugging purposes only
 
-  servo.attach(servoPO, sRange[0], sRange[1]);
-  //ESC.attach(escPO, escRange[0], escRange[1]);
+  servo.attach(servoPO);
+  //ESC.attach(escPO);
   servo.write(sRange[0]);
   //ESC.write(escRange[1]);
 } 
 
 void loop() {
-  servo.write(sRange[0]);
-  Serial.println("servo");
-  delay(500);
-  servo.write(sRange[1]);
-//    uint8_t buf[4];
-//    uint8_t buflen = sizeof(buf);
-//    if (drone.recv(buf, &buflen)) { // Non-blocking
-//      char bufNumber[3];
-//      for (int i = 0; i < buflen - 1; i++) {
-//        bufNumber[i] = buf[i];
-//      }
-//      int p = atoi(bufNumber);
-//      Serial.println(p);
-//    
-//      p = map(p, 0, 255, sRange[0], sRange[1]);
-//      Serial.println("writing to servo");
-//      servo.write(p);
-//      
-//      //p = map(p, 0, 1023, escRange[0], escRange[1]);
-//      //ESC.write(p);
-//      buf[0] = '0';
-//      buf[1] = '0';
-//      buf[2] = '0';
-//    }
+    uint8_t buf[4];
+    uint8_t buflen = sizeof(buf);
+    if (drone.recv(buf, &buflen)) { // Non-blocking
+      buf[3] = 0;
+      char bufNumber[3];
+      for (int i = 0; i < buflen - 1; i++) {
+        bufNumber[i] = buf[i];
+      }
+      int p = atoi(bufNumber);
+      Serial.println(p);
+    
+      p = map(p, 0, 255, sRange[0], sRange[1]);
+      //controlServo(p);
+      //Serial.println(p);
+      servo.write(p);
+      
+      //p = map(p, 0, 1023, escRange[0], escRange[1]);
+      //ESC.write(p);
+      buf[0] = '0';
+      buf[1] = '0';
+      buf[2] = '0';
+    }
+    delay(5);
 }
